bcbe8b895ee3274e046bd0f125caae509ed8a00d
prajun <prajunbudathoki1@gmail.com>
2026-03-23 20:57:11 +0545

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Footer } from "@/components/Footer";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({
   children,
@@ -8,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`h-full antialiased`}>
+    <html lang="en" className={cn("h-full", "antialiased", "font-sans", geist.variable)}>
       <body className="">
         <div className="mx-auto p-4 sm:max-w-xl md:max-w-3xl xl:max-w-7xl">
           <Navbar />
