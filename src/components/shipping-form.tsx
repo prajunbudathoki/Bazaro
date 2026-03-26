266cbf03e9b1be01dffcdb7a7a15a48cbb9c180f
prajun <prajunbudathoki1@gmail.com>
2026-03-26 22:19:39 +0545

    wip

--- src/components/shipping-form.tsx
+++ src/components/shipping-form.tsx
@@ -1,7 +1,47 @@
-export default function ShippingForm() {
+import { useForm } from "react-hook-form";
+import { ShippingFormInputs, shippingFormSchema } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+export default function ShippingForm({setShippingForm}: {}) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ShippingFormInputs>({
+    resolver: zodResolver(shippingFormSchema as any),
+  });
   return (
-    <div>
-      <h2>Shipping Form</h2>
-    </div>
+    <form className="flex flex-col gap-4">
+      <div className="grid grid-cols-2 gap-4">
+        <div className="flex flex-col gap-2">
+          <label htmlFor="name" className="text-sm font-medium">
+            Full Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            {...register("name")}
+            className="border border-gray-300 rounded-lg px-4 py-2"
+          />
+          {errors.name && (
+            <p className="text-sm text-red-500">{errors.name.message}</p>
+          )}
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="email" className="text-sm font-medium">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            {...register("email")}
+            className="border border-gray-300 rounded-lg px-4 py-2"
+          />
+          {errors.email && (
+            <p className="text-sm text-red-500">{errors.email.message}</p>
+          )}
+        </div>
+      </div>
+    </form>
   );
 }
